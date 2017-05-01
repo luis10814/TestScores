fa2da3264a56a0505bb8b639e30e54009b527e39
luis10814 <lp1172@yahoo.com>
2017-04-30 23:30:36 -0500

    test
    test

---
+++ src/testscores/TestScores.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testscores;
+import java.util.Scanner;
+import java.util.*;
+/**
+ *
+ * @author Luis
+ */
+public class TestScores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        for(int i = 0; i < 4; i++){
+        System.out.println("Enter test score: " + i+1);
+        
+        try{
+            
+        }
+        catch (IllegalArguementException e){
+            System.out.println();
+        }
+        
+        }
+        
+    }
+    
+}
